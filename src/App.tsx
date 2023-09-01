d8e609924a9dc5f2b342c910c083f8fe229c0ac2
Mina Isaac <isaac.mina.2010@gmail.com>
2023-09-01 12:49:00 +0200

    Create MatchList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from 'styled-components';
+import TeamList from './components/MatchList';
+import TopBar from './components/TopBar';
+import { AppContextProvider } from './AppContext';
+import ErrorOverlay from './components/ErrorOverlay';
+import './App.css';
+
+const AppWrapper = styled.div`
+  display: flex;
+  position: relative;
+  padding: 1rem;
+  flex-direction: column;
+  gap: 1.5rem;
+  align-items: center;
+  height: 100vh;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  background: #7dd3fc;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <AppContextProvider>
+      <AppWrapper>
+        <ErrorOverlay />
+        <TopBar />
+        <TeamList />
+      </AppWrapper>
+    </AppContextProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/MatchList.tsx
@@ -0,0 +1,145 @@
+import { useCallback, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Button, Input } from './common';
+import { useAppContext } from '../AppContext';
+
+const Container = styled.div`
+  display: flex;
+  overflow-y: auto;
+  position: relative;
+  padding: 1.2rem;
+  padding-top: 0;
+  flex-direction: column;
+  gap: 1rem;
+  border-radius: 0.5rem;
+  width: 100%;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  background-color: #ffffff;
+`;
+
+const Row = styled.div`
+  display: flex;
+  padding: 0.5rem;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 0.6rem;
+  box-shadow: inset #5f6690 0px 0px 5px 2px;
+  transition: all 0.2s;
+  &:hover {
+    transform: scale(1.016);
+  }
+`;
+
+const Title = styled.div`
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  padding: 1.5rem 0;
+  border-bottom: 2px solid #262626;
+  font-size: 1.875rem;
+  font-weight: 700;
+  text-align: center;
+  background-color: #ffffff;
+`;
+
+const TeamList: React.FC = () => {
+  const { scoreboard, boardUpdated, setBoardUpdated } = useAppContext();
+  const [boardSummary, setBoardSummary] = useState(scoreboard.getSummary);
+  const [selectedMatch, setSelectedMatch] = useState<number>();
+  const [updatedScore, setUpdatedScore] = useState<[number, number]>();
+
+  const handleScoreChange: React.ChangeEventHandler<HTMLInputElement> = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      const [homeScore, awayScore] = updatedScore!;
+      if (name === 'home') setUpdatedScore([Number(value), awayScore]);
+      if (name === 'away') setUpdatedScore([homeScore, Number(value)]);
+    },
+    [updatedScore],
+  );
+
+  const handleSave = (teams: [string, string]) => {
+    scoreboard.updateMatch(teams, updatedScore!);
+    setSelectedMatch(undefined);
+    setBoardUpdated(true);
+  };
+
+  const handleCancel = useCallback(() => setSelectedMatch(undefined), []);
+
+  useEffect(() => {
+    if (boardUpdated) {
+      setBoardSummary(scoreboard.getSummary());
+      setBoardUpdated(false);
+    }
+  }, [scoreboard, boardUpdated, setBoardUpdated]);
+
+  return (
+    <Container>
+      <Title>Ongoing Matches</Title>
+      {boardSummary.length === 0 && <div className='text-xl p-3'>No matches currently in progress</div>}
+      {boardSummary.map((summary, i) => {
+        const [homeSummary, awaySummary] = summary.split(' - ');
+        const [homeTeam, homeScore] = homeSummary.split(' ');
+        const [awayTeam, awayScore] = awaySummary.split(' ');
+
+        const MatchUpdate = (
+          <Row className="bg-slate-500">
+            <div className="flex gap-2">
+              <Input
+                type="number"
+                name="home"
+                value={updatedScore?.at(0)}
+                placeholder={homeTeam}
+                onChange={handleScoreChange}
+              />
+              <Input
+                type="number"
+                name="away"
+                value={updatedScore?.at(1)}
+                placeholder={awayTeam}
+                onChange={handleScoreChange}
+              />
+            </div>
+            <div className="flex gap-2">
+              <Button $bgColor="#6ee7b7" onClick={() => handleSave([homeTeam, awayTeam])}>
+                Save
+              </Button>
+              <Button $bgColor="#fca5a5" onClick={handleCancel}>
+                Cancel
+              </Button>
+            </div>
+          </Row>
+        );
+        const MatchSummary = (
+          <Row key={i}>
+            <span className="match-summary">{summary}</span>
+            <div className="flex gap-2">
+              <Button
+                $bgColor="#93c5fd"
+                onClick={() => {
+                  setSelectedMatch(i);
+                  setUpdatedScore([Number(homeScore), Number(awayScore)]);
+                }}
+              >
+                Update Scores
+              </Button>
+              <Button
+                $bgColor="#fca5a5"
+                onClick={() => {
+                  scoreboard.finishMatch(homeTeam, awayTeam);
+                  setBoardUpdated(true);
+                }}
+              >
+                Finish Match
+              </Button>
+            </div>
+          </Row>
+        );
+        return i === selectedMatch ? MatchUpdate : MatchSummary;
+      })}
+    </Container>
+  );
+};
+
+export default TeamList;
