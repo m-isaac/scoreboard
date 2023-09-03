dd8f01f23454b760bc083329b618683b00a84026
Mina Isaac <isaac.mina.2010@gmail.com>
2023-09-03 14:56:20 +0200

    Change AppContext implementation to expose only data needed by UI

--- src/AppContext/index.tsx
+++ src/AppContext/index.tsx
@@ -1,5 +1,12 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useState, PropsWithChildren, useContext } from 'react';
+import {
+  createContext,
+  useState,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useCallback,
+} from 'react';
 import ScoreBoard from '../utils/scoreBoard';
 import AppContext from './types';
 
@@ -9,13 +16,51 @@ const appContext = createContext<AppContext | null>(null);
 const AppContextProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const [error, setError] = useState<string>();
   const [boardUpdated, setBoardUpdated] = useState<boolean>(false);
+  const [boardSummary, setBoardSummary] = useState(scoreboard.getSummary);
+
+  useEffect(() => {
+    if (boardUpdated) {
+      setBoardSummary(scoreboard.getSummary());
+      setBoardUpdated(false);
+    }
+  }, [boardUpdated, setBoardUpdated]);
+
+  const startNewMatch = useCallback((homeTeam: string, awayTeam: string) => {
+    try {
+      scoreboard.startNewMatch(homeTeam, awayTeam);
+      setBoardUpdated(true);
+    } catch (e) {
+      if (e instanceof Error) setError(e.message);
+      setTimeout(() => setError(undefined), 1600);
+    }
+  }, []);
+
+  const finishMatch = useCallback((homeTeam: string, awayTeam: string) => {
+    try {
+      scoreboard.finishMatch(homeTeam, awayTeam);
+      setBoardUpdated(true);
+    } catch (e) {
+      if (e instanceof Error) setError(e.message);
+      setTimeout(() => setError(undefined), 1600);
+    }
+  }, []);
+
+  const updateMatch = useCallback((teams: [string, string], scores: [number, number]) => {
+    try {
+      scoreboard.updateMatch(teams, scores);
+      setBoardUpdated(true);
+    } catch (e) {
+      if (e instanceof Error) setError(e.message);
+      setTimeout(() => setError(undefined), 1600);
+    }
+  }, []);
 
   const contextValue: AppContext = {
-    scoreboard,
-    boardUpdated,
+    boardSummary,
     error,
-    setBoardUpdated,
-    setError,
+    startNewMatch,
+    finishMatch,
+    updateMatch,
   };
   return <appContext.Provider value={contextValue}>{children}</appContext.Provider>;
 };
